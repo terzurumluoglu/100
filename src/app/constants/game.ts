ffa9a04de4ab3fe32e79402823504770f6821f6d
Toprak <toprakerzurumluoglu@hotmail.com>
2021-02-25 10:50:01 +0300

    Update game.ts

--- src/app/constants/game.ts
+++ src/app/constants/game.ts
@@ -1,3 +1,3 @@
 export const k : number = 0.75;
 export const horizontal: number = 3; // Yatayda boşluk sayısı 
-export const cross: number = 2; // Yatayda boşluk sayısı 
+export const cross: number = 2; // Çaprazda boşluk sayısı 
